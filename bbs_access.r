017e1e8775cc7d5d96aaa749f103dd5864fb5e78
Lemon Doroshow <isaacdoroshow@gmail.com>
2026-05-21 13:24:59 -0700

    Start work on detection + occurence covars.

--- bbs_access.r
+++ bbs_access.r
@@ -1,4 +1,11 @@
+rm(list = ls())
 library(tidyverse)
+library(lubridate)
+library(sp)
+library(raster)
+library(FedData)
+library(sf)
+library(stars)
 
 # Load Mojave routes
 mojave_routes <- read.csv("./data/bbs_routes_mojave.csv")
@@ -14,7 +21,7 @@ weather <- read.csv("./data/bbs/Weather.csv") |>
   mutate(date = as.Date(date, tz = "America/New_York")) %>%
   mutate(julian = as.numeric(format(date, '%j')))
 
-# Import BBS data, and filter
+# Import BBS data, and filter (we will use 2024 as an "example year")
 bbs_obs_mojave <- read.csv("./data/bbs/States/Arizona.csv") %>%
   bind_rows(read.csv("./data/bbs/States/Califor.csv")) %>%
   bind_rows(read.csv("./data/bbs/States/Nevada.csv")) %>%
@@ -31,11 +38,21 @@ bbs_obs_mojave <- read.csv("./data/bbs/States/Arizona.csv") %>%
   filter(AOU == aou_cc) %>%
   left_join(weather, by = c("RouteDataID"))
 
-# %>%
-#   mutate(RouteNum = paste0(StateNum, formatC(Route, 2, flag = "0"))) %>%
-#   filter(RouteNum %in% mojave_routes$RTENO) %>%
-#   filter(AOU == aou_cc) %>%
-#   filter(Year >= 2001) %>%
-#   select(Year, RouteNum, SpeciesTotal) %>%
-#   arrange(Year) %>%
-#   mutate(x = route_xs[RouteNum], y = route_ys[RouteNum])
+# Create detection-nondetection matrix
+y <- as.matrix(bbs_obs_mojave[, c('Count10', 'Count20', 'Count30', 'Count40', 'Count50')])
+y <- ifelse(y > 0, 1, 0)
+
+# Compile detection covariates
+det_covs <- list(
+  day = c(scale(bbs_obs_mojave$julian)),
+  day.2 = c(scale(bbs_obs_mojave$julian) ^ 2),
+  tod = c(scale(bbs_obs_mojave$StartTime)),
+  obs = as.numeric(factor(bbs_obs_mojave$ObsN))
+)
+
+# Compile coordinates
+coords <- bbs_obs_mojave[, c('Latitude', 'Longitude')]
+coords_sp <- data.frame(coords, val = apply(y, 1, max)) %>%
+  arrange(Longitude, Latitude)
+coordinates(coords_sp) <- ~Longitude + Latitude
+proj4string(coords_sp) <- '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
