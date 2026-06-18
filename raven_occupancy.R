39b9dc6364ff1cbe6ae7cd81d717e0de3db66536
Lemon Doroshow <isaacdoroshow@gmail.com>
2026-06-18 14:36:05 -0700

    Run model with select interactions added

--- raven_occupancy.R
+++ raven_occupancy.R
@@ -29,10 +29,11 @@ phi.start <- p_file[p_file[, 1] == 'phi', 2]
 w.start <- p_file[p_file[, 1] == 'w', 2]
 
 # Specify model
-occ.formula <- ~ elevation + ndvi + impervious + roads + lines
+occ.formula <- ~ elevation + ndvi + impervious + roads + lines + 
+                  impervious * roads + impervious * lines
 det.formula <- ~ day + day.2 + tod + (1 | obs)
 p.det <- length(bbs_data$det.covs)
-p.occ <- ncol(bbs_data$occ.covs) + 1
+p.occ <- ncol(bbs_data$occ.covs) + 1 + 2 # covs + intercept + interactions
 dist.bbs <- dist(bbs_data$coords)
 mean.dist <- mean(dist.bbs)
 min.dist <- min(dist.bbs)
@@ -40,7 +41,7 @@ max.dist <- max(dist.bbs)
 inits <- list(alpha = rep(alpha.start, p.det),
               beta = rep(beta.start, p.occ),
               sigma.sq = sigma.sq.start, 
-              # phi = phi.start, 
+              # phi = phi.start, # This line was giving me an error?
               w = rep(w.start, nrow(bbs_data$y)),
               z = apply(bbs_data$y, 1, max, na.rm = TRUE))
 priors <- list(beta.normal = list(mean = rep(0, p.occ),
@@ -84,19 +85,24 @@ mojave_points <- rast(ext(mojave), res = 7500, crs = crs(mojave))
 # Open and resample covariates
 ndvi <- rast("./data/ndvi/processed/2024-05-30.tif") |> # Random date
   project("EPSG:5070") |>
-  resample(mojave_points)
+  resample(mojave_points) |>
+  scale()
 elev <- rast("./data/elevation/all_time.tif") |>
   project("EPSG:5070") |>
-  resample(mojave_points)
+  resample(mojave_points) |>
+  scale()
 roads <- rast("./data/roads/2024.tif") |>
   project("EPSG:5070") |>
-  resample(mojave_points)
+  resample(mojave_points) |>
+  scale()
 lines <- rast("./data/transmissionLines/all_time.tif") |>
   project("EPSG:5070") |>
-  resample(mojave_points)
+  resample(mojave_points) |>
+  scale()
 impervious <- rast("./data/impervious/processed/2024.tif") |>
   project("EPSG:5070") |>
-  resample(mojave_points)
+  resample(mojave_points) |>
+  scale()
 
 # Put covariates together
 covs <- as.data.frame(elev, xy = TRUE) |>
@@ -112,9 +118,16 @@ covs <- as.data.frame(elev, xy = TRUE) |>
   ) |> left_join(
     as.data.frame(lines, xy = TRUE),
     by = c("x", "y")
+  ) |> left_join(
+    as.data.frame(impervious * roads, xy = TRUE),
+    by = c("x", "y")
+  ) |> left_join(
+    as.data.frame(lines * roads, xy = TRUE),
+    by = c("x", "y")
   ) |>
   na.omit()
-colnames(covs) <- c("x", "y", "elevation", "ndvi", "impervious", "roads", "lines")
+colnames(covs) <- c("x", "y", "elevation", "ndvi", "impervious", "roads", "lines",
+                    "impervious_x_roads", "lines_x_roads")
 n_locs <- dim(covs)[1]
 
 # Prepare covariate matrix
@@ -138,4 +151,4 @@ colnames(occ_loc) <- c("x", "y", "psi")
 # Create prediction raster
 occ_rast <- as.data.frame(occ_loc) |>
   rast(crs = "EPSG:5070")
-writeRaster(occ_rast, paste0("./results/", run_str, ".tif"))
+writeRaster(occ_rast, paste0("./results/", run_str, ".tif"), overwrite = TRUE)
